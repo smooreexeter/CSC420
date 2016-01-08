3db2bd1fb195b974f93179dc54f5c63d4e464d0c
smooreexeter <smoore@exeter.edu>
2016-01-08 15:46:42 -0500

    Changes

--- Linked Lists 1/src/myLinkedList/Runner2.java
+++ Linked Lists 1/src/myLinkedList/Runner2.java
@@ -2,33 +2,37 @@ package myLinkedList;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-public class Runner {
+public class Runner2 {
 
 	public static void main(String[] args) {
-		//LinkedList<String> list = new LinkedList<String>();
 		Scanner sc = new Scanner(System.in);
-		LinkedList<MyLinkedList> mll = new LinkedList<MyLinkedList>();
-		mll.add(new MyLinkedList("Star Wars", null));
+		LList<String> ll = new LList<String>();
+		ll.append("Star Wars");
+		for(int i = 2; i < 6; i++){
+			ll.append("" + i);
+		}
 		boolean addMore = true;
 		while(addMore){
 			System.out.println("Type the name of the DVD, or press enter to print the list.");
 			String nameString = sc.nextLine();
 			if(!nameString.isEmpty()){
-				mll.add(new MyLinkedList(nameString, mll.get(mll.size()-1)));
+				ll.append(nameString);
 			} else{
 				addMore = false;
 			}
-		}
+		}/*
+		ll.moveToStart();
 		System.out.print("[");
-		for(int i = 0; i < mll.size(); i++){
-			System.out.print(mll.get(i).toString());
-			if(i+1!=mll.size()){
+		for(int i = 0; i < ll.length(); i++){
+			System.out.print(ll.getValue());
+			if(i+1!=ll.length()){
 				System.out.print(", ");
 			}
+			ll.next();
 		}
 		System.out.println("]");
-		//System.out.println(mll.toString());
-
+		*/
+		System.out.println(ll.toString());
 	}
 
 }
