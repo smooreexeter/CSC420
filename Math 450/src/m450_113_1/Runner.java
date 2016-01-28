736e546746e17749c81038992bcb822a7f3a293e
smooreexeter <smoore@exeter.edu>
2016-01-28 15:45:01 -0500

    stuff

--- Math 450/src/m450_113_1/Runner.java
+++ Math 450/src/m450_113_1/Runner.java
@@ -1,5 +1,7 @@
 package m450_113_1;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class Runner {
@@ -10,31 +12,36 @@ public class Runner {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		System.out.println(factorial(5));
-		double sumr = 0;
-		double sumt = 0;
-		double rat = 0;
-		double test = 0;
+		BigDecimal rat = new BigDecimal("0.0");
+		BigDecimal test = new BigDecimal("1");
+		BigDecimal last = new BigDecimal("1");
+		BigDecimal ten = new BigDecimal("1");
+		BigDecimal fac;
 		for(int i = 0; i <= n; i++){
+			fac = factorial(i);
 			System.out.println(i);
-			System.out.println("pow: " + Math.pow(10, i));
-			System.out.println("fac: " + factorial(i));
-			rat = Math.pow(10, i)/factorial(i);
-			sumr += rat;
-			System.out.println("---ratio: " + rat);
-			test = 1/Math.pow(2, i);
-			sumt += test;
+			System.out.println("pow: " + ten.toString());
+			System.out.println("fac: " + fac.toString());
+			rat = ten.divide(fac, 1000, RoundingMode.HALF_UP);
+			ten = ten.multiply(BigDecimal.TEN);
+			System.out.println("---10^n/n!: " + rat.stripTrailingZeros().toString());
+			test = test.multiply(new BigDecimal("0.5"));
 			System.out.println("+++1/2^n: " + test);
-			if(test > rat){
-				System.out.println("passed " + (sumr - sumt));
+			if(test.compareTo(rat)>0){
+				System.out.println("passed");
 			}
-			System.out.println("sum: " + sumr);
+			System.out.println("~~~Ratio n+1/n: " + rat.divide(last, 1000, RoundingMode.HALF_UP).stripTrailingZeros().toPlainString());
+			last = rat;
 		}
+		sc.close();
 	}
 	
-	public static double factorial(long a){
-		double ret = 1;
+	public static BigDecimal factorial(int a){
+		BigDecimal ret = new BigDecimal("1.0");
+		BigDecimal in;
 		for(int i = 1; i <= a; i++){
-			ret *= i;
+			in = new BigDecimal(Integer.toString(i));
+			ret = ret.multiply(in);
 		}
 		return ret;
 	}
